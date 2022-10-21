d36a97364b84d1194dd1254b6bd224e830c44c6f
omaru-ok <omix1.ok@gmail.com>
2022-10-22 01:30:35 +0530

    everythign from start :)

---
+++ WINDOW_terminal_UwU_0/OMI-window-terminal_conf-install.ps1
@@ -0,0 +1,6 @@
+write-host "												your terminal is now going to be lit ❤️‍🔥"
+
+
+
+
+irm https://github.com/omaru-ok/stuff/raw/main/Omi-window-terminal%20_conf_0.txt  -o ~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
