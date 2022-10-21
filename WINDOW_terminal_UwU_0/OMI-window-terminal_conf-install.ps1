de72e3eed1cdaf847869387b82cb66a81495e2a3
omaru-ok <omix1.ok@gmail.com>
2022-10-22 01:37:13 +0530

    link , issue fixed

--- WINDOW_terminal_UwU_0/OMI-window-terminal_conf-install.ps1
+++ WINDOW_terminal_UwU_0/OMI-window-terminal_conf-install.ps1
@@ -3,4 +3,4 @@ write-host "												your terminal is now going to be lit ❤️‍🔥"
 
 
 
-irm https://github.com/omaru-ok/stuff/raw/main/Omi-window-terminal%20_conf_0.txt  -o ~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+irm https://github.com/omaru-ok/stuff/raw/main/WINDOW_terminal_UwU_0/Omi-window-terminal%20_conf_0.txt -o ~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
