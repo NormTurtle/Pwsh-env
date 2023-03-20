e416d5558700d17846d348010f916b4fa9ab063b
omaru-ok <108952834+omaru-ok@users.noreply.github.com>
2023-03-21 01:47:25 +0530

    Update Microsoft.PowerShell_profile.ps1

--- Personal/Microsoft.PowerShell_profile.ps1
+++ Personal/Microsoft.PowerShell_profile.ps1
@@ -6,12 +6,15 @@ Import-Module Terminal-Icons
 Invoke-Expression (&starship init powershell)  
 Import-Module "~\scoop\modules\scoop-completion"
 #oh-my-posh init pwsh --config ~\scoop\apps\oh-my-posh\current\themes\pure.omp.json | iex 
-
+# Enviroment variables
+#$env:XDG_CONFIG_HOME="C:\Users\O_0\.config\"
+$env:ERDTREE_CONFIG_PATH="~/.config/erdtree/.erdtreerc"
 $EDITOR='nvim.exe'
+Set-Alias -Name oldvim -Value vim
 Set-Alias -Name vim -Value $EDITOR
 #Set-Alias -Name vim -Value nvim
 Set-Alias -Name nvide -Value neovide  
-Set-Alias -Name note -Value notepads
+#Set-Alias -Name note -Value notepads
 Set-Alias -Name mvp -Value mpv
 Set-Alias -Name mcube -Value musikcube
 Set-alias -Name brew -Value scoop 
@@ -22,7 +25,6 @@ Set-Alias -Name which -Value Get-Command
 Set-Alias -Name sd -Value speedtest 
 Set-Alias -Name cow  -Value cowsay
 #Set-Alias less ~\apps\git\current\usr\bin\less.exe'
-
 # PsReadLine
 Set-PSReadLineOption -EditMode Windows
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
@@ -42,10 +44,11 @@ $commandOverride = [ScriptBlock]{ param($Location) Write-Host $Location }
 Set-PsFzfOption -AltCCommand $commandOverride
 
 #Function
-#Function mpv  { ~/scoop/apps/mpv/current/mpv.com $args}
+Function mpe {~/scoop/apps/mpv/current/mpv.com $args}
+Function mvi {mpv --config-dir="C:\Users\O_0\scoop\apps\mpv-git\current\portable_config\mvi" $args }
+Function note {Start-Process notepads -ArgumenstList $args -NoNewWindow}
 #function mpd { mpd .\.config\mpd\mpd.conf -v & } 
 function gdi  { goodbyedpi & }
-Function et { C:\Users\O_0\local\erdtree\et-v1.2.0-x86_64-pc-windows-msvc.exe }
 #Function convert($cmd) {magick convert $cmd }
 # change dir fastly :D
 Function ..   { cd ..\ }
@@ -73,9 +76,9 @@ function md5    { Get-FileHash -Algorithm MD5 $args }
 # downlading yt-dlp with 3gp 
 #Function yt3gp ($url) { yt-dlp -a "${url}"  -o "%(title)s.%(ext)s" --exec 'ffmpeg -y -i {} -filter:v "scale=704x576" -c:v h263 -c:a aac -b:a 80k -ac 1 -ar 8000 {}.3gp && del /f {}' }
 Function yt3gp {
-    $urls = Get-Content -Path "file.txt"
+    $urls = Get-Content -Path "urls.txt"
     foreach ($url in $urls) {
-        yt-dlp -a "$url" -o "%(title)s.%(ext)s" --exec 'ffmpeg -y -i {} -filter:v "scale=704x576" -c:v h263 -c:a aac -b:a 80k -ac 1 -ar 8000 {}.3gp && del /f {}'
+        yt-dlp -a "$url" -o "%(title)s.%(ext)s" --exec 'ffmpeg -y -i {} -filter:v "scale=704x576" -c:v h263 -c:a aac -b:a 80k -ar 8000 {}.3gp && del /f {}'
     }
 }
 
@@ -88,12 +91,10 @@ function ffconvert {
         $output_file_path = Join-Path $output_folder $_.FullName.Substring($input_folder.Length + 1)
         $output_file_path = [IO.Path]::ChangeExtension($output_file_path, ".3gp")
         New-Item -ItemType Directory -Path (Split-Path $output_file_path) -Force | Out-Null
-        $cmd = "ffmpeg -y -i `"$($_.FullName)`" -vf scale=1408:1152 -c:v h263 -preset veryfast -crf 29 -c:a aac_mf -b:a 128k -ac 2 -ar 44100 `"$output_file_path`""
+        $cmd = "ffmpeg -y -i `"$($_.FullName)`" -vf scale=704:576 -c:v h263 -preset veryfast -crf 29 -c:a aac_mf -b:a 128k -ac 2 -ar 44100 `"$output_file_path`""
         Invoke-Expression $cmd
     }
 }
-
-
 
 function find-file($name) {
         ls -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | foreach {
