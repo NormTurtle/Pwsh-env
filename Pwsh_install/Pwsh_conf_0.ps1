a0ad9c0cd47a72832a5119705db21b2780e2e6d1
Omi <omix1.ok@gmail.com>
2023-06-04 17:45:37 +0530

    Revamped

--- Pwsh_install/Pwsh_conf_0.ps1
+++ Pwsh_install/Pwsh_conf_0.ps1
@@ -1,10 +1,8 @@
-
 # CONFIG_BY_OMI :) 
 
 #importFirst Modules
 Import-Module PSFzf
 Import-Module PSReadLine
-Import-Module Terminal-Icons
 Invoke-Expression (&starship init powershell)  
 Import-Module "~\scoop\modules\scoop-completion"
 
@@ -12,9 +10,8 @@ Import-Module "~\scoop\modules\scoop-completion"
 Set-Alias -Name ll -Value  lsd
 Set-Alias -Name which -Value Get-Command 
 Set-Alias grep ugrep 
-#Set-Alias less ~\apps\git\current\usr\bin\less.exe'
 
-# PsReadLine
+# PsReadLine   GOOD TAB Completion
 Set-PSReadLineOption -EditMode Windows
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineOption -PredictionSource History
@@ -24,3 +21,19 @@ Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
+# Functions   aka command alias for powershell
+Function hosts {gsudo nvim %windir%\system32\drivers\etc\hosts} # winow host files Netwrok related
+function pgrep($name) { ps $name}    # get runnig process info
+function pkill($name) { ps $name -ErrorAction SilentlyContinue | kill }  # kill any runnign process
+
+# Lazy Loading ICons
+Function Load-TerminalIcons {
+    if (-not (Get-Module -Name Terminal-Icons)) {
+        Import-Module Terminal-Icons
+    }
+}
+Function IconsDelayedLs {
+    Load-TerminalIcons
+    Get-ChildItem $args
+}
+Set-Alias -Name ls -Value IconsDelayedLs
